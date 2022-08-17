b23ce4890ae2404dc22712c4a1fe9d56a5a2c177
ehddjs95@naver.com <janghanjinn@naver.com>
2022-08-17 21:49:08 +0900

    2022.8.17 (반복문)

---
+++ Header2.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <stdio.h>
+
+//switch문
+/*
+void main()
+{
+	// 사분면
+	
+	// 제 1 사분면 : (+x, +y)
+	// 제 2 사분면 : (-x, +y)
+	// 제 3 사분면 : (-x, -y)
+	// 제 4 사분면 : (+x, -y)
+
+	// Y 절편 : (x[0], y)
+	// Y 절편 : (x, y[0])
+	// 원점 : (x[0], y[0])
+
+	int x = -2;
+	int y = 10;
+
+	if (x > 0 && y > 0)
+	{
+	   printf("제 1 사분면");
+	}
+	els if (x < 0 && y > 0)
+	{
+	   printf("제 2 사분면");
+	}
+	else if (x < 0 && y < 0)
+	{
+	   printf("제 3 사분면");
+	}
+	else if (x > 0 && y <0)
+	{
+	   printf("제 4 분면");
+	}
+	else if (x == 0 && y != 0)
+	{
+	   Printf("Y 절편");
+	}
+	else if (x != 0 && y == 0)
+	{
+	   printf("X 절편");
+	}
+	else
+	{
+	   printf("원점");
+	}
+	
+
+	// 논리 연산자
+	// 1 = 참
+	// 0 = 거짓
+	// && (AND) : 두개의 조건 모두 참이라면 1을 반환합니다.
+	// || (OR) : 두 개의 조건에 하나라도 참이면 조건을 실행합니다.
+
+	// switch
+	
+	// 어떤 결과에 따라 그 결과부터 실행되는 조건문입니다.
+
+
+
+	// break문
+	// 특정한 시점에서 분기를 탈출하는 제어문입니다.
+
+	int value = 0;
+
+	switch (value)
+	{
+	case 0: printf("0 입니다.");
+		break;
+	case 1: printf("1 입니다.");
+		break;
+	default: printf("잘못 선택했습니다.");
+	}
+
+
+	// 대학 성적표
+	// A는 100 ~ 90 점
+	// B는 90 ~80 점
+	// C는 80 ~ 70 점
+	// D는 70 ~ 60 점
+	// E는 60 ~ 50 점
+	// 과락입니다.
+
+	char alphabet = 'c';
+
+	switch (alphabet)
+	{
+		 case 'A': Printf("100점 ~ 90점");
+		   break;
+		 case 'B': Printf("90점 ~ 80점");
+		   brenk;
+		 case 'C': Printf("80점 ~ 70점");
+		   brenk;
+		 case 'D': Printf("70점 ~ 60점");
+		   brenk;
+		 case 'E: Printf("60점 ~ 50점");
+		   brenk;
+
+	float veriable = 3.25;
+
+
+	int x = 10;
+	int y = 20;
+
+	if (x == 10 && y == 20)
+	{
+		printf("논리 연산 AND가 참입니다.")
+	if (5 < 10)
+	}
+		printf(if문 안에는 조건문입니다.\n");
+
+	in (x == 10 || y ==25)
+	{
+	
+}
+*/
